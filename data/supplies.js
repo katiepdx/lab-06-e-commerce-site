783a9cab96f1bd6101fde41df74b3712049365b9
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-13 14:41:08 -0700

    add product images to ass and data

---
+++ data/supplies.js
@@ -0,0 +1,54 @@
+const spiralNotebook {
+    id: 'spiralnotebook543',
+    name: 'Spiral Notebook',
+    image: 'spiral-notebook-product.jpg',
+    description: 'A small A5 blank top spiral notebook',
+    category: 'paper-supplies',
+    price: 2.99
+};
+
+const balloon {
+    id: 'happyballoon235',
+    name: 'Yellow Balloon',
+    image: 'balloon-product.jpg',
+    description: 'A yellow happy face balloon',
+    category: 'balloons',
+    price: 0.25
+};
+
+const colorPencils {
+    id: 'colorpencilsl642',
+    name: 'Color Pencils',
+    image: 'pencil-product.jpg',
+    description: 'A set of color pencils',
+    category: 'writing-utensils',
+    price: 5.99
+};
+
+const colorPens {
+    id: 'colorpensl731',
+    name: 'Color Pens',
+    image: 'pens-product.jpg',
+    description: 'A set of color pens',
+    category: 'writing-utensils',
+    price: 3.99
+};
+
+const stickyNotes {
+    id: 'stickynotes437',
+    name: 'Sticky Notes',
+    image: 'sticky-note-product.jpg',
+    description: 'A set of color pens',
+    category: 'paper-supplies',
+    price: 3.99
+};
+
+const supplies = [
+    spiralNotebook,
+    balloon,
+    colorPencils,
+    colorPens,
+    stickyNotes
+];
+
+export default supplies
