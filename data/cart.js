045acc4ced1645ae3051f28c7aaf1b2ac2b3f790
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-14 13:26:35 -0700

    add cart data

---
+++ data/cart.js
@@ -0,0 +1,20 @@
+// hard-coded mock data
+// Array of objects in cart
+// Export itemsInCart data 
+
+export const itemsInCart = [{
+    id: 'spiralnotebook543',
+    quantity: 3
+}, {
+    id: 'happyballoon235',
+    quantity: 5
+}, {
+    id: 'colorpencilsl642',
+    quantity: 1
+}, {
+    id: 'colorpensl731',
+    quantity: 31
+}, {
+    id: 'stickynotes437',
+    quantity: 13
+}];
